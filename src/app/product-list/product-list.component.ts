78dbcc6470ad2325f1a5d2944b4dba4cd7403b37
Anu-cool-007 <anuranjan.mail@gmail.com>
2019-11-09 12:29:37 +0530

    Implemented events

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onAlertEvent(product) {
+    window.alert('Alert set for '+product.name);
+  }
 }
 
 
